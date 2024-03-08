386f3ab40eaa2c1a0b39b86b0b58defb04199fce
Samuel Simplicio <samuelsimplicio5@gmail.com>
2024-03-08 15:06:44 -0300

    Finished car movement inside the Vehicle namespace

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <cmath>
+#include <cstdio>
 #include <raylib.h>
+#include <string>
 #include <vector>
 #include "vehicle.hpp"
 
 
-const float SCALE = 5.0f;
+const float SCALE = 4.0f;
 const int WINDOW_WIDTH = 1000;
 const int WINDOW_HEIGHT = 600;
 const float ROAD_LENGTH = WINDOW_WIDTH / SCALE;
@@ -23,9 +25,9 @@ struct CirclePos
 };
 
 // draws the vehicle inside the circle with the right angle
-void draw_vehicle(CirclePos circle_position)
+void draw_vehicle(CirclePos circle_position, int i)
 {
-    Color color = { 255, 0, 255, 255 };
+    Color color = { 0, 0, 255, 255 };
     Rectangle rect = {
         SCALE * circle_position.pos.x,
         SCALE * circle_position.pos.y,
@@ -33,6 +35,7 @@ void draw_vehicle(CirclePos circle_position)
         SCALE * CAR_WIDE
     };
     DrawRectanglePro(rect, {0, 0}, 90 + 180 / PI * circle_position.angle, color);
+    DrawText(TextFormat("%i", i), rect.x, rect.y, 15, ORANGE);
 }
 
 
@@ -52,13 +55,53 @@ CirclePos pos_to_circle(Vec2 pos)
 }
 
 
+
+void draw_info(std::vector<Vehicle> vehicles)
+{
+    DrawText(TextFormat("Last car desired velocity: %.02f km/h", vehicles[vehicles.size()-1].v_d * 3.6), 10, 10, 20, RED);
+    DrawText(TextFormat("Last car velocity: %.02f km/h", vehicles[vehicles.size()-1].velocity * 3.6), 10, 30, 20, RED);
+}
+
+
+void input_desired_velocity(Vehicle& v)
+{
+    static std::string s = "";
+    if (IsKeyPressed(KEY_ONE)) s += "1";
+    if (IsKeyPressed(KEY_TWO)) s += "2";
+    if (IsKeyPressed(KEY_THREE)) s += "3";
+    if (IsKeyPressed(KEY_FOUR)) s += "4";
+    if (IsKeyPressed(KEY_FIVE)) s += "5";
+    if (IsKeyPressed(KEY_SIX)) s += "6";
+    if (IsKeyPressed(KEY_SEVEN)) s += "7";
+    if (IsKeyPressed(KEY_EIGHT)) s += "8";
+    if (IsKeyPressed(KEY_NINE)) s += "9";
+    if (IsKeyPressed(KEY_ZERO)) s += "0";
+    if (IsKeyPressed(KEY_BACKSPACE)) {
+        if (s.size() > 0) {
+            s = s.substr(0, s.size() - 1);
+        }
+    }
+    if (IsKeyPressed(KEY_PERIOD)) s += ".";
+    if (IsKeyPressed(KEY_ENTER)) {
+        v.v_d = std::stof(s) / 3.6;
+        s = "";
+    }
+
+    DrawText(TextFormat("Input %s", s.c_str()), 10, 50, 20, RED);
+}
+
+
 int main(void)
 {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "raylib [core] example - basic window");
-    int N = 10;
-    std::vector<Vehicle> vehicles = spawn_cars_near(N, ROAD_LENGTH);
+    int N = 18;
+    std::vector<Vehicle> vehicles = spawn_cars(N, ROAD_LENGTH);
 
     std::vector<CirclePos> circular_positions(N);
+    printf("\nInitial positions:\n");
+    for (Vehicle v: vehicles) {
+        printf("Pos X: %f, velocity: %f, acc: %f\n", v.position.x, v.velocity, v.acceleration);
+    }
     for (int i=0; i<N; i++)
     {
         circular_positions[i] = pos_to_circle(vehicles[i].position);
@@ -69,12 +112,12 @@ int main(void)
         // movement
         for (int i=0; i<N; i++)
         {
-            float pos = vehicles[i].position.x + 10.0f * dt;
+            vehicle::move_vehicle(i, vehicles, dt, ROAD_LENGTH);
+            Vehicle* v = &vehicles[i];
             // loop through the road, as its a circle
-            if (pos > ROAD_LENGTH) {
-                pos -= ROAD_LENGTH;
+            if (v->position.x > ROAD_LENGTH) {
+                v->position.x = 0;
             }
-            vehicles[i].position.x = pos;
             circular_positions[i] = pos_to_circle(vehicles[i].position);
         }
         BeginDrawing();
@@ -82,14 +125,15 @@ int main(void)
             ClearBackground(RAYWHITE);
             for (int i=0; i<N; i++)
             {
-                draw_vehicle(circular_positions[i]);
+                draw_vehicle(circular_positions[i], i);
             }
+            draw_info(vehicles);
+            input_desired_velocity(vehicles[vehicles.size()-1]);
         }
         EndDrawing();
         current_time = GetTime();
         dt = (float)(current_time - previous_time);
         previous_time = current_time;
-
     }
 
     CloseWindow();
--- vehicle.hpp
+++ vehicle.hpp
@@ -23,6 +23,7 @@ struct Vehicle
     Vec2 position;
     float acceleration;
     float velocity;
+    float v_d;
 };
 
 // everything is in meters
@@ -30,12 +31,12 @@ const float CAR_LENGTH = 4.9;
 const float CAR_WIDE = 1.94;
 
 // IDM params
-float a_max = 2.0f; // 0.8 to 2.5 m/s^2
+float a_max = 1.0f; // 0.8 to 2.5 m/s^2
 float a_dec = 2.0f; // around 2 m/s^2
 float delta_s_min = 2.0f; // m
 float v_d = 120.0f / 3.6; // m/s
 float mu = 4.0;
-float T = 1.8; // 0.8 to 2.0 s
+float T = 0.8; // 0.8 to 2.0 s
 float alpha = 0.99;
 
 
@@ -56,8 +57,9 @@ std::vector<Vehicle> spawn_cars(int N, float road_length)
     {
         Vec2 pos = { .x = i * x_step, .y = 20 };
         vehicles[i-1].position = pos;
-        vehicles[i-1].acceleration = randf() + 1.0f;
-        vehicles[i-1].velocity = v_d * (0.2f * randf() + 0.8f);
+        vehicles[i-1].acceleration = 0.2f * randf() + 1.0f;
+        vehicles[i-1].velocity = v_d * (0.05f * randf() + 0.95f);
+        vehicles[i-1].v_d = v_d;
     }
     return vehicles;
 }
@@ -66,13 +68,14 @@ std::vector<Vehicle> spawn_cars(int N, float road_length)
 std::vector<Vehicle> spawn_cars_near(int N, float road_length)
 {
     std::vector<Vehicle> vehicles(N);
-    float x_step = 1.1 * CAR_LENGTH;
+    float x_step = delta_s_min + CAR_LENGTH + 2.0f * randf();
     for (int i=0; i < N; i++)
     {
         Vec2 pos = { .x = i * x_step, .y = 20 };
         vehicles[i].position = pos;
-        vehicles[i].acceleration = randf() + 1.0f;
-        vehicles[i].velocity = v_d * (0.2f * randf() + 0.8f);
+        vehicles[i].acceleration = 0.2f * randf() + 1.0f;
+        vehicles[i].velocity = v_d * (0.05f * randf() + 0.95f);
+        vehicles[i].v_d = v_d;
     }
     return vehicles;
 }
@@ -80,10 +83,10 @@ std::vector<Vehicle> spawn_cars_near(int N, float road_length)
 
 // Intelligent Driver Model
 // https://traffic-simulation.de/info/info_IDM.html
-float IDM(float delta_s, float v, float delta_v)
+float IDM(float delta_s, float v, float delta_v, float v_d)
 {
     float delta_s_star = delta_s_min + v * T + (v * delta_v) / (2 * std::sqrt(a_max * a_dec));
-    float a_idm = a_max * (1 - std::pow(v / v_d, mu) - std::pow(delta_s_star, delta_s));
+    float a_idm = a_max * (1 - std::pow(v / v_d, mu) - std::pow(delta_s_star / delta_s, 2.0f));
     return a_idm;
 }
 
@@ -95,8 +98,8 @@ float CAH(float delta_s, float v, float v_l, float a_l)
     float delta_v = v - v_l;
     float a_tilde = std::min(a_l, a_max);
 
-    if (v_l * (delta_v) <= -2 * delta_s * a_tilde) {
-        return v * v / (v_l * v_l - 2 * delta_s * a_tilde);
+    if (v_l * delta_v <= -2 * delta_s * a_tilde) {
+        return (v * v * a_tilde) / (v_l * v_l - 2 * delta_s * a_tilde);
     } else {
         float step = delta_v < 0 ? 0 : 1;
         return a_tilde - (delta_v * delta_v * step) / (2 * delta_s);
@@ -106,9 +109,10 @@ float CAH(float delta_s, float v, float v_l, float a_l)
 
 // Adaptive Cruise Control
 // https://arxiv.org/pdf/0912.3613.pdf pages 5 and 6
-float ACC(float delta_s, float v, float delta_v, float v_l, float a_l)
+float ACC(float delta_s, float v, float v_l, float a_l, float v_d)
 {
-    float a_idm = IDM(delta_s, v, delta_v);
+    float delta_v = v - v_l;
+    float a_idm = IDM(delta_s, v, delta_v, v_d);
     float a_cah = CAH(delta_s, v, v_l, a_l);
 
     if (a_idm >= a_cah) {
@@ -130,6 +134,27 @@ float integrate_acc(float v, float acc, float dt)
 float integrate_v(float s, float v, float acc, float dt)
 {
     return s + v * dt + 0.5f * acc * dt * dt;
+}
+
+
+void move_vehicle(int vehicle_index, std::vector<Vehicle> &vehicles, float dt, float road_length)
+{
+    Vehicle vehicle = vehicles[vehicle_index];
+    Vehicle leader = vehicle_index == vehicles.size() - 1
+        ? vehicles[0] : vehicles[vehicle_index + 1];
+    // due to circular movement, delta_s could be negative, so we take
+    // the absolute value
+    float delta_s = (leader.position.x > vehicle.position.x
+        ? leader.position.x - vehicle.position.x
+        : road_length - vehicle.position.x + leader.position.x
+    ) - CAR_LENGTH;
+
+    float a = ACC(delta_s, vehicle.velocity, leader.velocity, leader.acceleration, vehicle.v_d);
+    float v = integrate_acc(vehicle.velocity, vehicle.acceleration, dt);
+    float s = integrate_v(vehicle.position.x, vehicle.velocity, vehicle.acceleration, dt);
+    vehicles[vehicle_index].acceleration = a;
+    vehicles[vehicle_index].velocity = v < 0 ? 0 : v;
+    vehicles[vehicle_index].position.x = s;
 }
 
 };
